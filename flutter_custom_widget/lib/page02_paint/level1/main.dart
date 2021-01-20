1dc1eadbdfdc29b2cd2e1f8159ce09d6cd5b7c27
tengfei <arrayadapter.cn@gmail.com>
2021-01-20 17:36:10 +0800

    update

---
+++ flutter_custom_widget/lib/page02_paint/level1/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_custom_widget/page02_paint/level1/paper.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft]);
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(Paper());
+}
